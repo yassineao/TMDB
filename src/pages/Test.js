cdbb5d2535d1913e6c53fda642d104231ac5d625
yassineao <yassuke4@gmail.com>
2024-05-18 00:32:50 +0200

    css

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -42,9 +42,11 @@ export default function Home() {
   var settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 1000, // Transition speed in milliseconds
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000, // Autoplay speed in milliseconds
   };
     return (
       <div>
