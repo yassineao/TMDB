dc243774754610fabf7de07139d08e280a0a5430
yassineao <yassuke4@gmail.com>
2024-05-18 19:32:31 +0200

    css

--- src/api/getMovies.js
+++ src/api/getMovies.js
@@ -1,7 +1,7 @@
 export const  getMovies = async () => {
     
 try {
-    const response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=831a4bb8a39f71fea9d3c2efe8fb5ab2');
+    const response = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&api_key=831a4bb8a39f71fea9d3c2efe8fb5ab2');
     const data = await response.json();
     return data
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -69,19 +69,19 @@ export default function Home() {
                 <header>
                 <Slider {...settings}>
                 {loading ? (
-        <p>Loading...</p>
-      ) : (
-        movies.results.slice(0, 5).map(movie => ( // Use slice(0, 20) to get only the first 20 movies
-        
-     <div>
-                  <Card Type={"movie"} movie={movie} />
+                    <p>Loading...</p>
+                  ) : (
+                    movies.results.slice(0, 5).map(movie => ( // Use slice(0, 20) to get only the first 20 movies
+                    
+                <div>
+                              <Card Type={"movie"} movie={movie} />
 
-      </div>
-     
-        ))
-      )}
-      
-    </Slider>
+                  </div>
+                
+                    ))
+                  )}
+                  
+                </Slider>
   
              
   
@@ -140,93 +140,32 @@ export default function Home() {
   
                     <section>
                       <header class="major">
-                        <h2>My Portfolio</h2>
+                        <h2>Popular movies now</h2>
                       </header>
                       <div class="row">
-                        <div class="col-4 col-6-medium col-12-small">
+                      {loading ? (
+                          <p>Loading...</p>
+                        ) : (
+                          moviesP.results.slice(0, 6).map(movie => ( // Use slice(0, 20) to get only the first 20 movies
+                          
+                                  
+                          
+
+                                    <div class="col-4 col-6-medium col-12-small">
                           <section class="box">
-                            <a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+                            <a href="#" class="image featured"><Cover Type="movie" Id={movie.id} number={1} /> </a>
                             <header>
-                              <h3>Ipsum feugiat et dolor</h3>
+                              <h3>{movie.title}</h3>
                             </header>
-                            <p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
-                            <footer>
-                              <ul class="actions">
-                                <li><a href="#" class="button alt">Find out more</a></li>
-                              </ul>
-                            </footer>
+                            <p>{movie.overview}</p>
+                          
                           </section>
                         </div>
-                        <div class="col-4 col-6-medium col-12-small">
-                          <section class="box">
-                            <a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
-                            <header>
-                              <h3>Sed etiam lorem nulla</h3>
-                            </header>
-                            <p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
-                            <footer>
-                              <ul class="actions">
-                                <li><a href="#" class="button alt">Find out more</a></li>
-                              </ul>
-                            </footer>
-                          </section>
-                        </div>
-                        <div class="col-4 col-6-medium col-12-small">
-                          <section class="box">
-                            <a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
-                            <header>
-                              <h3>Consequat et tempus</h3>
-                            </header>
-                            <p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
-                            <footer>
-                              <ul class="actions">
-                                <li><a href="#" class="button alt">Find out more</a></li>
-                              </ul>
-                            </footer>
-                          </section>
-                        </div>
-                        <div class="col-4 col-6-medium col-12-small">
-                          <section class="box">
-                            <a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
-                            <header>
-                              <h3>Blandit sed adipiscing</h3>
-                            </header>
-                            <p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
-                            <footer>
-                              <ul class="actions">
-                                <li><a href="#" class="button alt">Find out more</a></li>
-                              </ul>
-                            </footer>
-                          </section>
-                        </div>
-                        <div class="col-4 col-6-medium col-12-small">
-                          <section class="box">
-                            <a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a>
-                            <header>
-                              <h3>Etiam nisl consequat</h3>
-                            </header>
-                            <p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
-                            <footer>
-                              <ul class="actions">
-                                <li><a href="#" class="button alt">Find out more</a></li>
-                              </ul>
-                            </footer>
-                          </section>
-                        </div>
-                        <div class="col-4 col-6-medium col-12-small">
-                          <section class="box">
-                            <a href="#" class="image featured"><img src="images/pic07.jpg" alt="" /></a>
-                            <header>
-                              <h3>Dolore nisl feugiat</h3>
-                            </header>
-                            <p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
-                            <footer>
-                              <ul class="actions">
-                                <li><a href="#" class="button alt">Find out more</a></li>
-                              </ul>
-                            </footer>
-                          </section>
-                        </div>
+
+                      
+                          ))
+                        )}
+                       
                       </div>
                     </section>
   
