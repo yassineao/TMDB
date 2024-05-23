82c00d9c40e01d2226664fcd052d9647ef705936
yassineao <yassuke4@gmail.com>
2024-05-24 01:05:04 +0200

    css

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -5,9 +5,10 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Cover from '../components/Cover';
 import '../styles/test.css';
-import{fetchMovieDetails} from '../api/getMovie.js'
-import{fetchCastDetails, fetchSimilarMovies} from '../api/getActors.js'
-import Card from '../components/Card.js';
+import { fetchMovieDetails } from '../api/getMovie';
+import { fetchCastDetails, fetchSimilarMovies } from '../api/getActors';
+import Card from '../components/Card';
+
 function Test() {
     const location = useLocation();
     const params = new URLSearchParams(location.search);
@@ -16,9 +17,8 @@ function Test() {
     const [similarMovies, setSimilarMovies] = useState([]);
     const [cast, setCast] = useState([]);
     const Id = params.get('id');
-
     const t = params.get('Type');
-    
+
     const settings = {
         dots: true,
         infinite: true,
@@ -29,59 +29,65 @@ function Test() {
     };
 
     useEffect(() => {
-      const loadDetails = async () => {
-          try {
-              const movieData = await fetchMovieDetails(Id, t);
-              setItem(movieData);
-              
-              const castData = await fetchCastDetails(Id, t);
-              setCast(castData.cast);
-              const similarMoviesData = await fetchSimilarMovies(Id, t);
-              setSimilarMovies(similarMoviesData.results);
-              setLoading(false);
-          } catch (error) {
-              console.error('Error loading details:', error);
-          }
-      };
+        const loadDetails = async () => {
+            try {
+                const movieData = await fetchMovieDetails(Id, t);
+                setItem(movieData);
+                const castData = await fetchCastDetails(Id, t);
+                setCast(castData.cast);
+                const similarMoviesData = await fetchSimilarMovies(Id, t);
+                setSimilarMovies(similarMoviesData.results);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error loading details:', error);
+            }
+        };
 
-      loadDetails();
-  }, [Id, t]);
+        loadDetails();
+    }, [Id, t]);
 
     if (loading) {
         return <p>Loading...</p>;
     }
 
     return (
-        <div class="unique-container">
+        <div className="unique-container">
+            <style>
+                {`
+                .unique-container .hero::before {
+                    background-image: url('https://image.tmdb.org/t/p/original${item.backdrop_path}');
+                    background-size: cover;
+                }
+
+                .unique-container .hero {
+                    position: relative; /* Required to make the pseudo-element positioning work */
+                }
+                `}
+            </style>
             <section className="movie-card">
-
-                <div className="hero" >
-                {t === 'movie' ? (  <Cover Type="movie" Id={item.id} number={3} classN={"cover"}/>  ) : ( <Cover Type="tv" Id={item.id} number={1} classN={"cover"} />)}
-
-                    <div className="details">
-                        <div >                                {t === 'movie' ? (  <h2 className="title1">{item.title}</h2>) : ( <h2 className="title1">{item.name}</h2> )}
-                         <span>PG-13</span></div>
+                
+            <div className="details">
+                        {t === 'movie' ? (
+                            <h2 className="title1">{item.title}</h2>
+                        ) : (
+                            <h2 className="title1">{item.name}</h2>
+                        )}
+                        <span>PG-13</span>
                         <div className="title2">{item.tagline}</div>
-                        <fieldset className="rating">
-                            <input type="radio" id="star5" name="rating" value="5" /><label className="full" htmlFor="star5" title="Awesome - 5 stars"></label>
-                            <input type="radio" id="star4half" name="rating" value="4 and a half" /><label className="half" htmlFor="star4half" title="Pretty good - 4.5 stars"></label>
-                            <input type="radio" id="star4" name="rating" value="4" defaultChecked /><label className="full" htmlFor="star4" title="Pretty good - 4 stars"></label>
-                            <input type="radio" id="star3half" name="rating" value="3 and a half" /><label className="half" htmlFor="star3half" title="Meh - 3.5 stars"></label>
-                            <input type="radio" id="star3" name="rating" value="3" /><label className="full" htmlFor="star3" title="Meh - 3 stars"></label>
-                            <input type="radio" id="star2half" name="rating" value="2 and a half" /><label className="half" htmlFor="star2half" title="Kinda bad - 2.5 stars"></label>
-                            <input type="radio" id="star2" name="rating" value="2" /><label className="full" htmlFor="star2" title="Kinda bad - 2 stars"></label>
-                            <input type="radio" id="star1half" name="rating" value="1 and a half" /><label className="half" htmlFor="star1half" title="Meh - 1.5 stars"></label>
-                            <input type="radio" id="star1" name="rating" value="1" /><label className="full" htmlFor="star1" title="Sucks big time - 1 star"></label>
-                            <input type="radio" id="starhalf" name="rating" value="half" /><label className="half" htmlFor="starhalf" title="Sucks big time - 0.5 stars"></label>
-                        </fieldset>
-                       
+                  
                     </div>
+                <div className="hero">
+                    {t === 'movie' ? (
+                        <Cover Type="movie" Id={item.id} number={1} classN="cover" />
+                    ) : (
+                        <Cover Type="tv" Id={item.id} number={2} classN="cover" />
+                    )}
+
                 </div>
 
                 <div className="description">
-
                     <div className="column1">
-                        {item.genres.map(gen => (
+                        {item.genres.map((gen) => (
                             <span className="tag" key={gen.id}>{gen.name}</span>
                         ))}
                     </div>
@@ -89,38 +95,40 @@ function Test() {
                         <p className="text">{item.overview}</p>
                     </div>
                     <div id="extraDetails">
-                            <h1 className="extra">Original Language: {item.original_language}</h1>
-                            {t === 'movie' ? (  <h1 className="extra">Release Date: {item.release_date}</h1>) : ( <h1 className="extra">Release Date:{item.first_air_date}</h1> )}
+                        <h1 className="extra">Original Language: {item.original_language}</h1>
+                        {t === 'movie' ? (
+                            <h1 className="extra">Release Date: {item.release_date}</h1>
+                        ) : (
+                            <h1 className="extra">Release Date: {item.first_air_date}</h1>
+                        )}
 
-                            <h1 className="extra">Popularity: {item.popularity}</h1>
-                            <h1 className="extra">Vote Average: {item.vote_average}</h1>
-                        </div>
+                        <h1 className="extra">Popularity: {item.popularity}</h1>
+                        <h1 className="extra">Vote Average: {item.vote_average}</h1>
+                    </div>
                 </div>
             </section>
-           
+
             <h2 id="actors">Actors</h2>
             <section className="actors" id="style-5">
-            
-                
-                    {cast.slice(0, 30).map(actor => (
-                        <div key={actor.cast_id} className="actor-card">
-                            <img src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`} alt={actor.name} className="actor-image" />
-                            <h2 className="actor-name">{actor.name}</h2>
-                            <p className="actor-role">{actor.character}</p>
-                        </div>
-                    ))}
-                   
+                {cast.slice(0, 30).map((actor) => (
+                    <div key={actor.cast_id} className="actor-card">
+                        <img src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`} alt={actor.name} className="actor-image" />
+                        <h2 className="actor-name">{actor.name}</h2>
+                        <p className="actor-role">{actor.character}</p>
+                    </div>
+                ))}
             </section>
-            <section className="SFilm" >
-            <h2>Similar Movies</h2>
+
+            <section className="SFilm">
+                <h2>Similar Movies</h2>
                 <Slider {...settings}>
-                    {similarMovies.map(item => (
+                    {similarMovies.map((item) =>
                         t === 'movie' ? (
-                            <Card Type={"movie"} movie={item} />
-                           ) : (
-                            <Card Type={"serie"} serie={item} movie={null}/>
-                          )
-                    ))}
+                            <Card Type="movie" movie={item} key={item.id} />
+                        ) : (
+                            <Card Type="serie" serie={item} movie={null} key={item.id} />
+                        )
+                    )}
                 </Slider>
             </section>
         </div>
