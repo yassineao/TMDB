9594aff8e60488c9d6964ae80d0b79c64b3d2fd7
yassineao <yassuke4@gmail.com>
2024-07-01 19:17:13 +0200

    css

--- src/pages/Genre.js
+++ src/pages/Genre.js
@@ -1,6 +1,7 @@
 
 import React, { useEffect, useState } from 'react';
 
+import GlitchingButton from '../components/glitchingButton';
 import Genre from '../components/genre';
 export default function Genres() {
   const [genres, setGenres] = useState([]);
@@ -8,7 +9,7 @@ export default function Genres() {
   const [error, setError] = useState(null);
   const [genreId, setGenreId] = useState(null); // Initialize genreId to null
   const [showPopup, setShowPopup] = useState(false);
-
+  const [searchType, setSearchType] = useState('movie');
   const handleTogglePopup = (id) => {
     setGenreId(id);
     setShowPopup((prev) => !prev);
@@ -20,6 +21,7 @@ export default function Genres() {
         const response = await fetch(
           'https://api.themoviedb.org/3/genre/movie/list?api_key=831a4bb8a39f71fea9d3c2efe8fb5ab2&language=en-US'
         );
+        console.log(searchType);
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
@@ -39,16 +41,21 @@ export default function Genres() {
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <div id='genre'>
-      {genres.map(genre => (
-        <Genre 
-          key={genre.id}
-          genreId={genre.id}
-          name={genre.name}
-          handleTogglePopup={handleTogglePopup}
-          showPopup={showPopup && genreId === genre.id}
-        />
-      ))}
+    <div >     
+      <div id='uni'>       
+      <GlitchingButton handleClick={() => setSearchType(searchType === 'movie' ? 'tv' : 'movie')} name={searchType} aria={true} />
+      </div>    
+     <div id='genre'>
+          {genres.map(genre => (
+            <Genre 
+              key={genre.id}
+              genreId={genre.id}
+              name={genre.name}
+              handleTogglePopup={handleTogglePopup}
+              showPopup={showPopup && genreId === genre.id}
+            />
+          ))}
+        </div>
     </div>
   );
 }
