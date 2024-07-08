b370ce3a95dd74bd7a0575f5e6ecf0e31ac7bec7
yassineao <yassuke4@gmail.com>
2024-07-08 16:15:19 +0200

    css

--- src/components/movieDetails.js
+++ src/components/movieDetails.js
@@ -4,19 +4,19 @@ const MovieDetails = ({ item, t }) => (
     <section className="redaktion">
         <ul className="redaktion-list">
             <div id="">
-                <h2 className="red">Original Language <h1 className="extra">{item.original_language}</h1></h2>
+                <h2 className="red">Original Language : <h1 className="extra">{item.original_language}</h1></h2>
                 {t === 'movie' ? (
-                    <h2 className="red">Release Date <h1 className="extra">{item.release_date}</h1></h2>
+                    <h2 className="red">Release Date : <h1 className="extra">{item.release_date}</h1></h2>
                 ) : (
-                    <h2 className="red">Release Date <h1 className="extra">{item.first_air_date}</h1></h2>
+                    <h2 className="red">Release Date : <h1 className="extra">{item.first_air_date}</h1></h2>
                 )}
-                <h2 className="red">Popularity <h1 className="extra">{item.popularity}</h1></h2>
-                <h2 className="red">Vote Average <h1 className="extra">{item.vote_average}</h1></h2>
+                <h2 className="red">Popularity : <h1 className="extra">{item.popularity}</h1></h2>
+                <h2 className="red">Vote Average : <h1 className="extra">{item.vote_average}</h1></h2>
                 {t === 'movie' && (
                     <>
-                        <h2 className="red">Runtime <h1 className="extra">{item.runtime} minutes</h1></h2>
-                        <h2 className="red">Budget <h1 className="extra">${item.budget.toLocaleString()}</h1></h2>
-                        <h2 className="red">Revenue <h1 className="extra">${item.revenue.toLocaleString()}</h1></h2>
+                        <h2 className="red">Runtime : <h1 className="extra">{item.runtime} minutes</h1></h2>
+                        <h2 className="red">Budget : <h1 className="extra">${item.budget.toLocaleString()}</h1></h2>
+                        <h2 className="red">Revenue :<h1 className="extra">${item.revenue.toLocaleString()}</h1></h2>
                     </>
                 )}
                 {item.production_companies && item.production_companies.length > 0 && (
